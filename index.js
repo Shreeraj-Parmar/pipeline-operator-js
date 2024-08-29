5e8ed03c5869c813f36d12b2a2d5389c402eb60d
Shreeraj Parmar <147899890+Shreeraj-Parmar@users.noreply.github.com>
2024-08-29 23:12:08 +0530

    1st

---
+++ index.js
@@ -0,0 +1,8 @@
+const double = (x) => x * 2;
+const square = (x) => x * x;
+let result = 3 |> double |> square |> increese;
+
+console.log(result);
+const increese = (x) => x + 1;
+
+console.log("hii");
